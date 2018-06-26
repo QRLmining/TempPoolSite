74e638bfdf980b1a9d4011dd25ebe7c768cbb28c
root <root@ip-172-31-4-65.us-west-1.compute.internal>
2018-06-26 14:21:27 +0000

    Adding Site content

---
+++ config.js
@@ -0,0 +1,11 @@
+var api = "http://qrl.com:8117";
+var coinUnits = 1000000000000;
+var poolHost = "qrlmining.com";
+var irc = "irc.freenode.net/#monero-pools";
+var email = "support@qrlmining.com";
+var cryptonatorWidget = ["QRL-BTC", "QRL-USD", "QRL-EUR", "QRL-GBP"];
+var easyminerDownload = "https://github.com/fireice-uk/xmr-stak/releases";
+var blockchainExplorer = "https://explorer.theqrl.org/block/";
+var transactionExplorer = "https://explorer.theqrl.org/tx/";
+
+
