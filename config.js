e81a963c5336e85a4b540a317155546e3d6d1061
qrlmining <qrlmining@qrlmining.com>
2018-07-30 15:43:38 +0000

    Updates to the pool site

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var api = "http://qrlmining.com:8117";
 var coinUnits = 1000000000;
 var poolHost = "qrlmining.com";
-var irc = "irc.freenode.net/#monero-pools";
+var irc = "irc.freenode.net/#qrl-pools";
 var email = "support@qrlmining.com";
 var cryptonatorWidget = ["QRL-BTC", "QRL-USD", "QRL-EUR", "QRL-GBP"];
 var easyminerDownload = "https://github.com/fireice-uk/xmr-stak/releases";
-var blockchainExplorer = "https://explorer.theqrl.org/block/";
-var transactionExplorer = "https://explorer.theqrl.org/tx/";
+var blockchainExplorer = "http://explorer.qrlmining.info/block/";
+var transactionExplorer = "http://explorer.qrlmining.info/tx/";
 
 
